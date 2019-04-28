b2232e63bbcd480df0dd94f5d1258bfead2cba6c
Andrew D. Zonenberg <azonenberg@drawersteak.com>
2019-04-28 16:23:08 -0700

    Initial version of function generator test

---
+++ fgtest/main.cpp
@@ -0,0 +1,93 @@
+/***********************************************************************************************************************
+*                                                                                                                      *
+* ANTIKERNEL v0.1                                                                                                      *
+*                                                                                                                      *
+* Copyright (c) 2012-2019 Andrew D. Zonenberg                                                                          *
+* All rights reserved.                                                                                                 *
+*                                                                                                                      *
+* Redistribution and use in source and binary forms, with or without modification, are permitted provided that the     *
+* following conditions are met:                                                                                        *
+*                                                                                                                      *
+*    * Redistributions of source code must retain the above copyright notice, this list of conditions, and the         *
+*      following disclaimer.                                                                                           *
+*                                                                                                                      *
+*    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the       *
+*      following disclaimer in the documentation and/or other materials provided with the distribution.                *
+*                                                                                                                      *
+*    * Neither the name of the author nor the names of any contributors may be used to endorse or promote products     *
+*      derived from this software without specific prior written permission.                                           *
+*                                                                                                                      *
+* THIS SOFTWARE IS PROVIDED BY THE AUTHORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED   *
+* TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL *
+* THE AUTHORS BE HELD LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES        *
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR       *
+* BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT *
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE       *
+* POSSIBILITY OF SUCH DAMAGE.                                                                                          *
+*                                                                                                                      *
+***********************************************************************************************************************/
+
+/**
+	@file
+	@author Andrew D. Zonenberg
+	@brief Program entry point
+ */
+
+#include "../scopehal/scopehal.h"
+#include "../scopehal/Instrument.h"
+#include "../scopehal/FunctionGenerator.h"
+#include "../scopehal/LeCroyVICPOscilloscope.h"
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	Severity console_verbosity = Severity::NOTICE;
+
+	//Parse command-line arguments
+	string hostname;
+	for(int i=1; i<argc; i++)
+	{
+		string s(argv[i]);
+
+		//Let the logger eat its args first
+		if(ParseLoggerArguments(i, argc, argv, console_verbosity))
+			continue;
+
+		if(s == "--help")
+		{
+			//not implemented
+			return 0;
+		}
+		else if(s == "--host")
+			hostname = argv[++i];
+		else
+		{
+			fprintf(stderr, "Unrecognized command-line argument \"%s\", use --help\n", s.c_str());
+			return 1;
+		}
+	}
+
+	//Set up logging
+	g_log_sinks.emplace(g_log_sinks.begin(), new ColoredSTDLogSink(console_verbosity));
+
+	LeCroyVICPOscilloscope scope(hostname, 1861);
+	if(0 == (scope.GetInstrumentTypes() & Instrument::INST_FUNCTION))
+	{
+		LogError("not a function generator\n");
+		return 1;
+	}
+
+	scope.SetFunctionChannelActive(0, true);
+	for(int i=0; i<50; i++)
+	{
+		for(float mhz=0.5; mhz<5; mhz += 0.01)
+		{
+			scope.SetFunctionChannelFrequency(0, mhz * 1.0e6f);
+			usleep(1000 * 50);
+		}
+	}
+
+
+	return 0;
+}
