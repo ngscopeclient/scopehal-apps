5aaa9e801d3258ca0cfe03d15301ddfe359b67ea
Andrew D. Zonenberg <azonenberg@drawersteak.com>
2020-02-12 20:58:00 -0800

    ScopeThread: cleanly return if a capture fails

--- glscopeclient/main.cpp
+++ glscopeclient/main.cpp
@@ -2,7 +2,7 @@
 *                                                                                                                      *
 * ANTIKERNEL v0.1                                                                                                      *
 *                                                                                                                      *
-* Copyright (c) 2012-2019 Andrew D. Zonenberg                                                                          *
+* Copyright (c) 2012-2020 Andrew D. Zonenberg                                                                          *
 * All rights reserved.                                                                                                 *
 *                                                                                                                      *
 * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the     *
@@ -316,7 +316,12 @@ void ScopeThread(Oscilloscope* scope)
 
 		if(stat == Oscilloscope::TRIGGER_MODE_TRIGGERED)
 		{
-			scope->AcquireData(true);
+			//Collect the data, fail if that doesn't work
+			if(!scope->AcquireData(true))
+			{
+				tlast = GetTime();
+				continue;
+			}
 
 			//Measure how long the acquisition took
 			double now = GetTime();
