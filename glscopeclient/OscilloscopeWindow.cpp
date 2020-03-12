3ebb00f3368c51b11f9da1d5dd4188e603fe3a02
Andrew D. Zonenberg <azonenberg@drawersteak.com>
2020-03-12 16:14:26 -0700

    Fixed non-digital decodes not being packed into the waveform box

--- glscopeclient/OscilloscopeWindow.cpp
+++ glscopeclient/OscilloscopeWindow.cpp
@@ -1004,6 +1004,8 @@ WaveformArea* OscilloscopeWindow::DoAddChannel(OscilloscopeChannel* chan, Wavefo
 
 	if(chan->GetType() == OscilloscopeChannel::CHANNEL_TYPE_DIGITAL)
 		ngroup->m_waveformBox.pack_start(*w, Gtk::PACK_SHRINK);
+	else
+		ngroup->m_waveformBox.pack_start(*w);
 
 	//Move the new trace after the reference trace, if one was provided
 	if(ref != NULL)
