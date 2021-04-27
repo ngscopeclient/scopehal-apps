e8fbf444879588aee7cb3e6a1409e6f69af92f40
Andrew Zonenberg <azonenberg@drawersteak.com>
2021-04-27 00:00:09 -0700

    Fixed clipping bug in spectrogram shader

--- src/glscopeclient/shaders/spectrogram-fragment.glsl
+++ src/glscopeclient/shaders/spectrogram-fragment.glsl
@@ -14,7 +14,6 @@ void main()
 	//Look up the intensity value and clamp it
 	vec4 yvec = texture(fbtex, vec2(texcoord));
 	float y = yvec.r;
-	if( (texcoord.x < 0) || (texcoord.y > 1) )
 	if(y >= 0.99)
 		y = 0.99;
 	if(y < 0.001)
