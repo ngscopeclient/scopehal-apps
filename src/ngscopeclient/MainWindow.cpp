4ce6b5eeef878b8cd473de3f2547d3cc142a1bda
Andrew D. Zonenberg <azonenberg@drawersteak.com>
2022-10-05 20:16:03 -0700

    Fixed sign/unsigned warning

--- src/ngscopeclient/MainWindow.cpp
+++ src/ngscopeclient/MainWindow.cpp
@@ -818,7 +818,7 @@ void MainWindow::AddToRecentInstrumentList(SCPIInstrument* inst)
 	m_recentInstruments[connectionString] = now;
 
 	//Delete anything old
-	const int maxRecentInstruments = m_session.GetPreferences().GetInt("Miscellaneous.Menus.recent_instrument_count");
+	size_t maxRecentInstruments = m_session.GetPreferences().GetInt("Miscellaneous.Menus.recent_instrument_count");
 	while(m_recentInstruments.size() > maxRecentInstruments)
 	{
 		string oldestPath = "";
