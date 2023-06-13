35197c5dd20b364e79d908fc71815988e2396833
Andrew D. Zonenberg <azonenberg@drawersteak.com>
2023-06-13 07:05:41 -0700

    MetricsDialog in ngscopeclient now displays pinned and local memory budgets if VK_EXT_memory_budget is available. See #523.

--- src/ngscopeclient/MetricsDialog.cpp
+++ src/ngscopeclient/MetricsDialog.cpp
@@ -206,6 +206,80 @@ bool MetricsDialog::DoRender()
 		}
 	}
 
+	//Only show this tab if available
+	if(g_hasMemoryBudget)
+	{
+		if(ImGui::CollapsingHeader("Memory"))
+		{
+			//TODO: host OS (un-pinned) memory usage too?
+			auto properties = g_vkComputePhysicalDevice->getMemoryProperties2<
+				vk::PhysicalDeviceMemoryProperties2,
+				vk::PhysicalDeviceMemoryBudgetPropertiesEXT>();
+			auto membudget = std::get<1>(properties);
+
+			Unit bytes(Unit::UNIT_BYTES);
+			Unit pct(Unit::UNIT_PERCENT);
+
+			auto pinnedUsage = membudget.heapUsage[g_vkPinnedMemoryHeap];
+			auto pinnedBudget = membudget.heapBudget[g_vkPinnedMemoryHeap];
+
+			auto localUsage = membudget.heapUsage[g_vkLocalMemoryHeap];
+			auto localBudget = membudget.heapBudget[g_vkLocalMemoryHeap];
+
+			if(ImGui::TreeNodeEx("Pinned", ImGuiTreeNodeFlags_DefaultOpen))
+			{
+				ImGui::BeginDisabled();
+					str = bytes.PrettyPrint(pinnedBudget, 4);
+					ImGui::SetNextItemWidth(10 * ImGui::GetFontSize());
+					ImGui::InputText("Budget", &str);
+				ImGui::EndDisabled();
+
+				HelpMarker(
+					"Amount of pinned CPU-side RAM available for use by ngscopeclient.\n\n"
+					"This is your total RAM minus memory which cannot be pinned for PCIe access,\n"
+					"or is in use by the OS or other applications.");
+
+				ImGui::BeginDisabled();
+					str = bytes.PrettyPrint(pinnedUsage, 4) +
+						" (" + pct.PrettyPrint(pinnedUsage * 1.0f / pinnedBudget, 4) + ")";
+					ImGui::SetNextItemWidth(10 * ImGui::GetFontSize());
+					ImGui::InputText("Usage", &str);
+				ImGui::EndDisabled();
+
+				HelpMarker("Amount of pinned CPU-side RAM currently in use by ngscopeclient.");
+
+				ImGui::TreePop();
+			}
+
+			if(ImGui::TreeNodeEx("Local", ImGuiTreeNodeFlags_DefaultOpen))
+			{
+				ImGui::BeginDisabled();
+					str = bytes.PrettyPrint(localBudget, 4);
+					ImGui::SetNextItemWidth(10 * ImGui::GetFontSize());
+					ImGui::InputText("Budget", &str);
+				ImGui::EndDisabled();
+
+				HelpMarker(
+					"Amount of GPU-side RAM available for use by ngscopeclient.\n\n"
+					"This is your total video RAM minus memory which is in use by the OS or other applications.\n"
+					"For unified-memory systems, this is normally the same as your pinned memory budget."
+					);
+
+				ImGui::BeginDisabled();
+					str = bytes.PrettyPrint(localUsage, 4) +
+						" (" + pct.PrettyPrint(localUsage * 1.0f / localBudget, 4) + ")";
+					ImGui::SetNextItemWidth(10 * ImGui::GetFontSize());
+					ImGui::InputText("Usage", &str);
+				ImGui::EndDisabled();
+
+				HelpMarker("Amount of GPU-side RAM currently in use by ngscopeclient.");
+
+				ImGui::TreePop();
+			}
+
+		}
+	}
+
 	return true;
 }
 
