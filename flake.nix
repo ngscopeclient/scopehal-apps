2bd1180b06db10724153a84764d47f54a2e3d658
Hans Baier <foss@hans-baier.de>
2024-03-20 06:25:15 +0700

    nix flake: fix devshell to be able to actually build the project

--- flake.nix
+++ flake.nix
@@ -38,8 +38,11 @@
       devShell = forAllSystems (system:
         let
           pkgs = nixpkgsFor.${system};
+          scopeHal = packages.${system}.scopehal-apps;
         in pkgs.mkShell {
-          buildInputs = [ packages.${system}.scopehal-apps ];
+          nativeBuildInputs = scopeHal.nativeBuildInputs;
+          buildInputs = scopeHal.buildInputs;
+          hardeningDisable = [ "all" ];
           shellHook = "echo Welcome to the ngscopeclient devShell!\n" + 
                       packages.${system}.libraryPaths;
         }
