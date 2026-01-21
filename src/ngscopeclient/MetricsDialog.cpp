c4f048a5d36622d15ebeaf56e613a02e87464507
Andrew D. Zonenberg <azonenberg@drawersteak.com>
2026-01-21 07:03:46 -0800

    MetricsDialog: removed redundant session pointer since base dialog class now has this

--- src/ngscopeclient/MetricsDialog.cpp
+++ src/ngscopeclient/MetricsDialog.cpp
@@ -1,8 +1,8 @@
 /***********************************************************************************************************************
 *                                                                                                                      *
-* glscopeclient                                                                                                        *
+* ngscopeclient                                                                                                        *
 *                                                                                                                      *
-* Copyright (c) 2012-2023 Andrew D. Zonenberg                                                                          *
+* Copyright (c) 2012-2026 Andrew D. Zonenberg and contributors                                                         *
 * All rights reserved.                                                                                                 *
 *                                                                                                                      *
 * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the     *
@@ -43,8 +43,7 @@ using namespace std;
 // Construction / destruction
 
 MetricsDialog::MetricsDialog(Session* session)
-	: Dialog("Performance Metrics", "Metrics", ImVec2(300, 400))
-	, m_session(session)
+	: Dialog("Performance Metrics", "Metrics", ImVec2(300, 400), session)
 {
 	m_displayRefreshRate = 0;
 
--- src/ngscopeclient/MetricsDialog.h
+++ src/ngscopeclient/MetricsDialog.h
@@ -1,8 +1,8 @@
 /***********************************************************************************************************************
 *                                                                                                                      *
-* glscopeclient                                                                                                        *
+* ngscopeclient                                                                                                        *
 *                                                                                                                      *
-* Copyright (c) 2012-2023 Andrew D. Zonenberg                                                                          *
+* Copyright (c) 2012-2026 Andrew D. Zonenberg and contributors                                                         *
 * All rights reserved.                                                                                                 *
 *                                                                                                                      *
 * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the     *
@@ -30,7 +30,7 @@
 /**
 	@file
 	@author Andrew D. Zonenberg
-	@brief Declaration of ChannelPropertiesDialog
+	@brief Declaration of MetricsDialog
  */
 #ifndef MetricsDialog_h
 #define MetricsDialog_h
@@ -46,7 +46,6 @@ public:
 	virtual bool DoRender();
 
 protected:
-	Session* m_session;
 
 	int m_displayRefreshRate;
 };
