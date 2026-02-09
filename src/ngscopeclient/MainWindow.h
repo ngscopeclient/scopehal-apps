d43f827d89f01a8f222e05ba284963b6d0ce9c0d
fredzo <fborry@free.fr>
2026-02-09 18:24:31 +0100

    Finisehd merge.

--- src/ngscopeclient/MainWindow.h
+++ src/ngscopeclient/MainWindow.h
@@ -70,10 +70,26 @@ public:
 	, m_direction(direction)
 	, m_stream(stream)
 	, m_ramp(ramp)
+	, m_streamGroup(nullptr)
 	{
 		auto schan = dynamic_cast<OscilloscopeChannel*>(stream.m_channel);
 		if(schan)
 			schan->AddRef();
+	}
+
+	SplitGroupRequest(std::shared_ptr<WaveformGroup> group, ImGuiDir direction, std::shared_ptr<StreamGroupDescriptor> streamGroup, bool singleArea)
+	: m_group(group)
+	, m_direction(direction)
+	, m_streamGroup(streamGroup)
+	, m_singleArea(singleArea)
+	{
+		if(streamGroup)
+		{
+			for(auto chan : streamGroup->m_channels)
+			{
+				chan->AddRef();
+			}
+		}
 	}
 
 	SplitGroupRequest(const SplitGroupRequest& rhs)
@@ -81,19 +97,41 @@ public:
 	, m_direction(rhs.m_direction)
 	, m_stream(rhs.m_stream)
 	, m_ramp(rhs.m_ramp)
+	, m_streamGroup(rhs.m_streamGroup)
+	, m_singleArea(rhs.m_singleArea)
 	{
-		auto schan = dynamic_cast<OscilloscopeChannel*>(rhs.m_stream.m_channel);
-		if(schan)
-			schan->AddRef();
+		if(rhs.m_stream)
+		{
+			auto schan = dynamic_cast<OscilloscopeChannel*>(rhs.m_stream.m_channel);
+			if(schan)
+				schan->AddRef();
+		}
+		else if(rhs.m_streamGroup)
+		{
+			for(auto chan : rhs.m_streamGroup->m_channels)
+			{
+				chan->AddRef();
+			}
+		}
 	}
 
 	SplitGroupRequest& operator=(const SplitGroupRequest& /*rhs*/) =delete;
 
 	~SplitGroupRequest()
 	{
-		auto schan = dynamic_cast<OscilloscopeChannel*>(m_stream.m_channel);
-		if(schan)
-			schan->Release();
+		if(m_stream)
+		{
+			auto schan = dynamic_cast<OscilloscopeChannel*>(m_stream.m_channel);
+			if(schan)
+				schan->Release();
+		}
+		else if(m_streamGroup)
+		{
+			for(auto chan : m_streamGroup->m_channels)
+			{
+				chan->Release();
+			}
+		}
 	}
 
 	std::shared_ptr<WaveformGroup> m_group;
@@ -102,6 +140,9 @@ public:
 
 	///@brief Color ramp request (may be blank if unspecified)
 	std::string m_ramp;
+	
+	std::shared_ptr<StreamGroupDescriptor> m_streamGroup;
+	bool m_singleArea;
 };
 
 /**
@@ -141,6 +182,9 @@ public:
 		StreamDescriptor stream,
 		std::string colorRamp)
 	{ m_splitRequests.push_back(SplitGroupRequest(group, direction, stream, colorRamp)); }
+
+	void QueueSplitGroup(std::shared_ptr<WaveformGroup> group, ImGuiDir direction, std::shared_ptr<StreamGroupDescriptor> streamGroup, bool singleArea)
+	{ m_splitRequests.push_back(SplitGroupRequest(group, direction, streamGroup, singleArea)); }
 
 	void ShowChannelProperties(OscilloscopeChannel* channel);
 	void ShowInstrumentProperties(std::shared_ptr<Instrument> instrument);
