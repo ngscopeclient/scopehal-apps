3d98482c5c6dfd43e9b820fc8308077f2f90d20c
Andrew D. Zonenberg <azonenberg@drawersteak.com>
2026-03-01 15:00:08 -0800

    Added test driver script

---
+++ test-scripts/test-driver-debian.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+apt install build-essential git cmake pkgconf libgtk-3-dev libsigc++-2.0-dev libyaml-cpp-dev catch2 libglfw3-dev curl xzip libhidapi-dev lsb-release dpkg-dev file libvulkan-dev glslang-dev glslang-tools spirv-tools glslc liblxi-dev libtirpc-dev texlive texlive-fonts-extra texlive-extra-utils
+git clone --recursive https://github.com/ngscopeclient/scopehal-apps
+rm -rf build
+mkdir build
+ctest -S ctest-custom.cmake
