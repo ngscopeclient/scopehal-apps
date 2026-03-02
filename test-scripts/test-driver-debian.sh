cb98f1c651a97c6c192fe9c367f216e661690d39
Andrew D. Zonenberg <azonenberg@drawersteak.com>
2026-03-01 21:39:46 -0800

    auto-yes in test script

--- test-scripts/test-driver-debian.sh
+++ test-scripts/test-driver-debian.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-sudo apt install build-essential git cmake pkgconf libgtk-3-dev libsigc++-2.0-dev libyaml-cpp-dev catch2 libglfw3-dev curl xzip libhidapi-dev lsb-release dpkg-dev file libvulkan-dev glslang-dev glslang-tools spirv-tools glslc liblxi-dev libtirpc-dev texlive texlive-fonts-extra texlive-extra-utils
+sudo apt -y update
+sudo apt -y full-upgrade
+sudo apt -y install build-essential git cmake pkgconf libgtk-3-dev libsigc++-2.0-dev libyaml-cpp-dev catch2 libglfw3-dev curl xzip libhidapi-dev lsb-release dpkg-dev file libvulkan-dev glslang-dev glslang-tools spirv-tools glslc liblxi-dev libtirpc-dev texlive texlive-fonts-extra texlive-extra-utils
 rm -rf build
 mkdir build
 ctest -S ctest-custom.cmake
