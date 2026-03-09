392da0ea8d6046ada1f9e9fd627d98ed557855be
Andrew D. Zonenberg <azonenberg@drawersteak.com>
2026-03-08 21:13:40 -0700

    Added VulkanSanityCheck unit test to print Vulkan information and just verify the Vulkan infrastructure is sane

---
+++ tests/Vulkan/main.cpp
@@ -0,0 +1,47 @@
+/***********************************************************************************************************************
+*                                                                                                                      *
+* ngscopeclient                                                                                                    *
+*                                                                                                                      *
+* Copyright (c) 2012-2026 Andrew D. Zonenberg and contributors                                                         *
+* All rights reserved.                                                                                                 *
+*                                                                                                                      *
+* Redistribution and use in source and binary forms, with or without modification, are permitted provided that the     *
+* following conditions are met:                                                                                        *
+*                                                                                                                      *
+*    * Redistributions of source code must retain the above copyright notice, this list of conditions, and the         *
+*      following disclaimer.                                                                                           *
+*                                                                                                                      *
+*    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the       *
+*      following disclaimer in the documentation and/or other materials provided with the distribution.                *
+*                                                                                                                      *
+*    * Neither the name of the author nor the names of any contributors may be used to endorse or promote products     *
+*      derived from this software without specific prior written permission.                                           *
+*                                                                                                                      *
+* THIS SOFTWARE IS PROVIDED BY THE AUTHORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED   *
+* TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL *
+* THE AUTHORS BE HELD LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES        *
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR       *
+* BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT *
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE       *
+* POSSIBILITY OF SUCH DAMAGE.                                                                                          *
+*                                                                                                                      *
+***********************************************************************************************************************/
+
+#include "scopehal.h"
+
+using namespace std;
+
+int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[])
+{
+	g_log_sinks.push_back(make_unique<ColoredSTDLogSink>(Severity::DEBUG));
+
+	if(!VulkanInit())
+		return 1;
+
+	LogDebug("Vulkan initialization successful\n");
+
+	//Shut everything down cleanly, otherwise we get dangling stuff and destructors called in unpredictable orders
+	//and the test might segfault or otherwise spuriously fail
+	ScopehalStaticCleanup();
+	return 0;
+}
