509cdf0385558dd47ab09b32512c6b763af34407
Andrew D. Zonenberg <azonenberg@drawersteak.com>
2026-04-25 03:42:57 -0700

    oops we borked a multiline shell command

--- test-scripts/test-driver-windows.sh
+++ test-scripts/test-driver-windows.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 pacman -Sy --needed --noconfirm \
-	git wget mingw-w64-ucrt-x86_64-cmake mingw-w64-ucrt-x86_64-toolchain
+	git wget mingw-w64-ucrt-x86_64-cmake mingw-w64-ucrt-x86_64-toolchain \
 	mingw-w64-ucrt-x86_64-libsigc++ mingw-w64-ucrt-x86_64-yaml-cpp mingw-w64-ucrt-x86_64-glfw \
 	mingw-w64-ucrt-x86_64-catch mingw-w64-ucrt-x86_64-hidapi mingw-w64-ucrt-x86_64-libpng \
 	mingw-w64-ucrt-x86_64-vulkan-headers mingw-w64-ucrt-x86_64-vulkan-loader \
