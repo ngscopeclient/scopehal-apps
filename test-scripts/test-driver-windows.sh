f01317145158f3c379ebcc57ed14ed879e4eb946
Andrew D. Zonenberg <azonenberg@drawersteak.com>
2026-04-25 03:29:35 -0700

    Override binary/source dir on windows

--- test-scripts/test-driver-windows.sh
+++ test-scripts/test-driver-windows.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
-pacman -Sy --needed --noconfirm git wget mingw-w64-ucrt-x86_64-cmake mingw-w64-ucrt-x86_64-toolchain
-pacman -Sy --needed --noconfirm mingw-w64-ucrt-x86_64-libsigc++ mingw-w64-ucrt-x86_64-yaml-cpp mingw-w64-ucrt-x86_64-glfw mingw-w64-ucrt-x86_64-catch mingw-w64-ucrt-x86_64-hidapi mingw-w64-ucrt-x86_64-libpng
-pacman -Sy --needed --noconfirm mingw-w64-ucrt-x86_64-vulkan-headers mingw-w64-ucrt-x86_64-vulkan-loader mingw-w64-ucrt-x86_64-shaderc mingw-w64-ucrt-x86_64-glslang mingw-w64-ucrt-x86_64-spirv-tools
-pacman -Sy --needed --noconfirm mingw-w64-ucrt-x86_64-fftw
+pacman -Sy --needed --noconfirm \
+	git wget mingw-w64-ucrt-x86_64-cmake mingw-w64-ucrt-x86_64-toolchain
+	mingw-w64-ucrt-x86_64-libsigc++ mingw-w64-ucrt-x86_64-yaml-cpp mingw-w64-ucrt-x86_64-glfw \
+	mingw-w64-ucrt-x86_64-catch mingw-w64-ucrt-x86_64-hidapi mingw-w64-ucrt-x86_64-libpng \
+	mingw-w64-ucrt-x86_64-vulkan-headers mingw-w64-ucrt-x86_64-vulkan-loader \
+	mingw-w64-ucrt-x86_64-shaderc mingw-w64-ucrt-x86_64-glslang mingw-w64-ucrt-x86_64-spirv-tools \
+	mingw-w64-ucrt-x86_64-fftw
 ctest -S ctest-custom.cmake
