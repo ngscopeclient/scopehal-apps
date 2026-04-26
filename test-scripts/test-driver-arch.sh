1cc2900e0c59b9b44f024255b1680c97bf67f4dc
Andrew D. Zonenberg <azonenberg@drawersteak.com>
2026-04-25 21:54:14 -0700

    Initial arch test driver

---
+++ test-scripts/test-driver-arch.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+pacman -Syu --noconfirm
+	git \
+	gcc \
+	cmake \
+	make \
+	pkgconf \
+	gtk3 \
+	libsigc++ \
+	yaml-cpp \
+	catch2 \
+	glfw \
+	curl \
+	hidapi \
+	ccache \
+	ninja \
+	vulkan-swrast \
+	lsb-release \
+	fftw \
+	texlive-bin \
+	texlive-binextra \
+	texlive-luatex \
+	texlive-plaingeneric \
+	texlive-latex \
+	texlive-latexrecommended \
+	texlive-latexextra \
+	texlive-fontsextra \
+	spirv-headers \
+	vulkan-headers \
+	vulkan-icd-loader \
+	shaderc \
+	glslang
+
+ctest -S ctest-custom.cmake
