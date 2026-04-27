c70671cfca4a5cc53aac4e6787c005f2540fd149
Andrew D. Zonenberg <azonenberg@drawersteak.com>
2026-04-27 01:48:17 -0700

    CMAKE_PREFIX_PATH update for homebrew

--- ctest-custom.cmake
+++ ctest-custom.cmake
@@ -124,7 +124,7 @@ elseif(${HOSTNAME} STREQUAL "macos.cidmz.poulsbo.antikernel.net" )
 	message(STATUS "Found known CI config: macos")
 
 	set(CTEST_BUILD_CONFIGURATION "RelWithDebInfo")
-	set(CONFIGURE_OPTIONS "-DBUILD_TESTING=ON -DBUILD_DOCS=OFF -DCMAKE_PREFIX_PATH=/opt/homebrew/opt/libomp")
+	set(CONFIGURE_OPTIONS "-DBUILD_TESTING=ON -DBUILD_DOCS=OFF -DCMAKE_PREFIX_PATH=/opt/homebrew;/opt/homebrew/opt/libomp")
 
 else()
 
