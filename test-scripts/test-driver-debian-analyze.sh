823c01fdd858b730d2539edde2551d5aa0d36352
Andrew D. Zonenberg <azonenberg@drawersteak.com>
2026-05-07 11:41:47 -0700

    Initial version of static analysis scripts for CI. Fixes #240.

---
+++ test-scripts/test-driver-debian-analyze.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+sudo apt -y update
+sudo apt -y full-upgrade
+sudo apt -y install build-essential git cmake pkgconf libgtk-3-dev libsigc++-2.0-dev libyaml-cpp-dev catch2 libglfw3-dev curl xzip libhidapi-dev lsb-release dpkg-dev file libvulkan-dev glslang-dev glslang-tools spirv-tools glslc liblxi-dev libtirpc-dev texlive texlive-binaries texlive-fonts-extra texlive-extra-utils libfftw3-dev vulkan-validationlayers cppcheck clang-tools
+
+export VK_LOADER_LAYERS_ENABLE=VK_LAYER_KHRONOS_validation
+source ./test-scripts/Validation.sh
+export ANALYZE=1
+ctest -S ctest-custom.cmake
