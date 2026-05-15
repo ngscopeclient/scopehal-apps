8c990c01799c88cb18829884711df3290f500597
Andrew D. Zonenberg <azonenberg@drawersteak.com>
2026-05-14 20:16:28 -0700

    Windows: copy wix build log off

--- test-scripts/test-driver-windows.sh
+++ test-scripts/test-driver-windows.sh
@@ -14,7 +14,8 @@ cd build
 cpack
 
 # Copy the package to the output path
-mkdir ~/artifacts
-mv *.msi ~/artifacts/
-mv doc/*.pdf ~/artifacts/
-cp C:/Users/ci/scopehal-apps/build/_CPack_Packages/win64/WIX/wix.log ~/artifacts/
+ARTIFACT_PATH=c:/Users/ci/artifacts/
+mkdir ARTIFACT_PATH
+mv *.msi ARTIFACT_PATH
+mv doc/*.pdf ARTIFACT_PATH
+cp C:/Users/ci/scopehal-apps/build/_CPack_Packages/win64/WIX/wix.log ARTIFACT_PATH
