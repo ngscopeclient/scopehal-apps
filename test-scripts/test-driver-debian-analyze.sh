954e2f937862c481af62b14bb96a15d93838852f
Andrew D. Zonenberg <azonenberg@drawersteak.com>
2026-05-16 12:11:00 -0700

    Try adding exclusions to scan-build to suppress findings from third party libraries

--- test-scripts/test-driver-debian-analyze.sh
+++ test-scripts/test-driver-debian-analyze.sh
@@ -4,4 +4,13 @@ sudo apt -y full-upgrade
 sudo apt -y install build-essential git cmake pkgconf libgtk-3-dev libsigc++-2.0-dev libyaml-cpp-dev catch2 libglfw3-dev curl xzip libhidapi-dev lsb-release dpkg-dev file libvulkan-dev glslang-dev glslang-tools spirv-tools glslc liblxi-dev libtirpc-dev texlive texlive-binaries texlive-fonts-extra texlive-extra-utils libfftw3-dev vulkan-validationlayers cppcheck clang-tools libomp-dev clang
 
 export ANALYZE=1
-scan-build --use-cc=clang --use-c++=clang++ ctest -S ctest-custom.cmake
+
+# Run the actual static analysis build
+scan-build \
+	--use-cc=clang --use-c++=clang++ \
+	--exclude lib/canvas_ity \
+	--exclude src/imgui \
+	--exclude src/imgui_markdown \
+	--exclude src/imgui-node-editor \
+	--exclude src/nativefiledialog-extended \
+	ctest -S ctest-custom.cmake
